0c928bb8c8cff71d5d86d19fb00a707ab4d46c56
laurita <laura.bledaite@gmail.com>
2013-01-12 17:44:24 +0100

    change sample trips in bootstrap

--- bootstrap.sql
+++ bootstrap.sql
@@ -2,20 +2,20 @@ INSERT INTO trips VALUES (
     "Brixen, Italy", "Rovereto, Italy",
     46.70288030, 11.69295620,
     45.85758350, 11.02945110,
-    "2012-12-29",
+    "2013-01-23",
     "laura@gmail.com"
 );
 INSERT INTO trips VALUES (
     "Laives, Italy", "Trento, Italy",
     46.42711360, 11.33729140,
     46.06969240, 11.12108860,
-    "2013-01-01",
+    "2013-01-23",
     "barsukas@gmail.com"
 );
 INSERT INTO trips VALUES (
     "Bolzano, Italy", "Rovereto",
     46.49829530, 11.35475820,
     45.85758350, 11.02945110,
-    "2014-01-01",
+    "2014-01-23",
     "seskas@gmail.com"
 );
